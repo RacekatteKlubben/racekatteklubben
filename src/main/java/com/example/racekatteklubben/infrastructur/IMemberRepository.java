f7457dea66c973c79fc5631259f18a916aeb6cc9
Tobiasrolesen@gmail.com <Tobiasrolesen@gmail.com>
2026-04-14 13:29:25 +0200

    Changes by tag

--- src/main/java/com/example/racekatteklubben/infrastructur/IMemberRepository.java
+++ src/main/java/com/example/racekatteklubben/infrastructur/IMemberRepository.java
@@ -1,9 +1,14 @@
 package com.example.racekatteklubben.infrastructur;
 
 import com.example.racekatteklubben.domain.Member;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 public interface IMemberRepository {
     Member findMemberByEmail(String email);
     void updateMember(Member member);
     void createMember(Member member);
+    List<Member> memberSearch(String query);
+    void deleteMember(int memberId);
 }
